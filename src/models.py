60d16034c1c23a0775a8a974ccb17bea16c8f165
Peter Asenov <peter12345@abv.bg>
2019-07-08 14:57:46 +0300

    start using namedtuples in models.py

--- src/models.py
+++ src/models.py
@@ -1,23 +1,10 @@
+from collections import namedtuple
+
 __all__ = ['KeyPairModel', 'KeyModel', 'ServiceModel']
 
+KeyPairModel = namedtuple('KeyPairModel', ('public_key', 'private_key'))
 
-class KeyPairModel:
-    def __init__(self, public_key, private_key):
-        self.public_key = public_key
-        self.private_key = private_key
+KeyModel = namedtuple('KeyModel', ('alias', 'signature_type', 'controller', 'public_key', 'private_key'),
+                      defaults=(None,))
 
-
-class KeyModel:
-    def __init__(self, alias, signature_type, controller, public_key, private_key=None):
-        self.alias = alias
-        self.signature_type = signature_type
-        self.controller = controller
-        self.public_key = public_key
-        self.private_key = private_key
-
-
-class ServiceModel:
-    def __init__(self, service_type, endpoint, alias):
-        self.service_type = service_type
-        self.endpoint = endpoint
-        self.alias = alias
+ServiceModel = namedtuple('ServiceModel', ('service_type', 'endpoint', 'alias'))
