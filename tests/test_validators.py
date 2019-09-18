1fe1043d7c52f7a6805509507c8dab41e0b72260
Valentin Ganev <sanchopansa@gmail.com>
2019-09-18 14:28:15 +0300

    add DIDUpdate entry unit tests

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -10,8 +10,11 @@ from client.constants import DID_METHOD_NAME
 from client.enums import DIDKeyPurpose, SignatureType
 from client.keys import DIDKey, ManagementKey
 from client.service import Service
-from resolver.exceptions import MalformedDIDManagementEntry
-from resolver.validators import validate_did_management_entry_format
+from resolver.exceptions import MalformedDIDManagementEntry, MalformedDIDUpdateEntry
+from resolver.validators import (
+    validate_did_management_entry_format,
+    validate_did_update_entry_format,
+)
 
 
 def load_json_schema(filename, version="1.0.0"):
@@ -39,10 +42,6 @@ class TestDIDManagementEntryValidation:
     )
 
     def test_insufficient_extids(self):
-        with pytest.raises(MalformedDIDManagementEntry) as excinfo:
-            validate_did_management_entry_format([], "", self.DID_MANAGEMENT_VALIDATOR)
-        assert str(excinfo.value) == "DIDManagement entry must have at least two ExtIDs"
-
         with pytest.raises(MalformedDIDManagementEntry) as excinfo:
             validate_did_management_entry_format(
                 ["DIDManagement"], "", self.DID_MANAGEMENT_VALIDATOR
@@ -139,4 +138,240 @@ class TestDIDManagementEntryValidation:
         }
         validate_did_management_entry_format(
             ["DIDManagement", "1.0.0"], valid_entry, self.DID_MANAGEMENT_VALIDATOR
+        )
+
+
+class TestDIDUpdateEntryValidation:
+    DID_UPDATE_VALIDATOR = get_validator(load_json_schema("did_update_entry.json"))
+
+    def test_insufficient_extids(self):
+        with pytest.raises(MalformedDIDUpdateEntry) as excinfo:
+            validate_did_update_entry_format(
+                [
+                    "DIDUpdate",
+                    "1.0.0",
+                    "{}:{}#my-key".format(DID_METHOD_NAME, secrets.token_hex(32)),
+                ],
+                "",
+                self.DID_UPDATE_VALIDATOR,
+            )
+        assert str(excinfo.value) == "DIDUpdate entry must have at least four ExtIDs"
+
+    def test_malformed_extids(self):
+        key_id = "{}:{}#{}".format(
+            DID_METHOD_NAME, secrets.token_hex(32), "my-man-key-1"
+        )
+        with pytest.raises(MalformedDIDUpdateEntry) as excinfo:
+            validate_did_update_entry_format(
+                ["DIdUpdate", "1.0.0", key_id, "0xaf01"], "", self.DID_UPDATE_VALIDATOR
+            )
+        assert str(excinfo.value) == "First ExtID of DIDUpdate entry must be DIDUpdate"
+
+        with pytest.raises(MalformedDIDUpdateEntry) as excinfo:
+            validate_did_update_entry_format(
+                ["DIDUpdate", "1.0.", key_id, "0xaf01"], "", self.DID_UPDATE_VALIDATOR
+            )
+        assert (
+            str(excinfo.value)
+            == "Second ExtID of DIDUpdate entry must be a semantic version number"
+        )
+
+        with pytest.raises(MalformedDIDUpdateEntry) as excinfo:
+            validate_did_update_entry_format(
+                ["DIDUpdate", "1.0.0", key_id[: key_id.find("#")], "0xaf01"],
+                "",
+                self.DID_UPDATE_VALIDATOR,
+            )
+        assert (
+            str(excinfo.value)
+            == "Third ExtID of DIDUpdate entry must be a valid full key identifier"
+        )
+
+        with pytest.raises(MalformedDIDUpdateEntry) as excinfo:
+            validate_did_update_entry_format(
+                ["DIDUpdate", "1.0.0", key_id, "aff0"], "", self.DID_UPDATE_VALIDATOR
+            )
+        assert (
+            str(excinfo.value)
+            == "Fourth ExtID of DIDUpdate entry must be a hex string with leading 0x"
+        )
+
+        with pytest.raises(MalformedDIDUpdateEntry) as excinfo:
+            validate_did_update_entry_format(
+                ["DIDUpdate", "1.0.0", key_id, "0xaffz"], "", self.DID_UPDATE_VALIDATOR
+            )
+        assert (
+            str(excinfo.value)
+            == "Fourth ExtID of DIDUpdate entry must be a hex string with leading 0x"
+        )
+
+    def test_invalid_entry(self):
+        did = "{}:{}".format(DID_METHOD_NAME, secrets.token_hex(32))
+        key_id = "{}#{}".format(did, "my-man-key-1")
+
+        # Entry with invalid property names
+        with pytest.raises(MalformedDIDUpdateEntry) as excinfo:
+            validate_did_update_entry_format(
+                ["DIDUpdate", "1.0.0", key_id, "0xaffe"],
+                {
+                    "added": {
+                        "managementKey": [
+                            ManagementKey(
+                                alias="my-man-key-1",
+                                controller=did,
+                                signature_type=SignatureType.EdDSA.value,
+                                priority="0",
+                            ).to_entry_dict(did)
+                        ]
+                    }
+                },
+                self.DID_UPDATE_VALIDATOR,
+            )
+        assert str(excinfo.value) == "Malformed DIDUpdate entry content"
+
+        # Entry with invalid property names
+        with pytest.raises(MalformedDIDUpdateEntry) as excinfo:
+            validate_did_update_entry_format(
+                ["DIDUpdate", "1.0.0", key_id, "0xaffe"],
+                {
+                    "add": {
+                        "managementKey": [
+                            ManagementKey(
+                                alias="my-man-key-1",
+                                controller=did,
+                                signature_type=SignatureType.EdDSA.value,
+                                priority="0",
+                            ).to_entry_dict(did)
+                        ]
+                    },
+                    "remove": {"managementKey": [{"id": "management-1"}]},
+                },
+                self.DID_UPDATE_VALIDATOR,
+            )
+        assert str(excinfo.value) == "Malformed DIDUpdate entry content"
+
+        # Entry with additional properties
+        with pytest.raises(MalformedDIDUpdateEntry) as excinfo:
+            validate_did_update_entry_format(
+                ["DIDUpdate", "1.0.0", key_id, "0xaffe"],
+                {
+                    "add": {
+                        "managementKey": [
+                            ManagementKey(
+                                alias="my-man-key-1",
+                                controller=did,
+                                signature_type=SignatureType.EdDSA.value,
+                                priority="0",
+                            ).to_entry_dict(did)
+                        ]
+                    },
+                    "revoke": {"managementKey": [{"id": "management-1"}]},
+                    "additional": {},
+                },
+                self.DID_UPDATE_VALIDATOR,
+            )
+        assert str(excinfo.value) == "Malformed DIDUpdate entry content"
+
+    def test_valid_entry(self):
+        did = "{}:{}".format(DID_METHOD_NAME, secrets.token_hex(32))
+        key_id = "{}#{}".format(did, "my-man-key-1")
+
+        # Empty entry content should be valid
+        validate_did_update_entry_format(
+            ["DIDUpdate", "1.0.0", key_id, "0xaffe"], {}, self.DID_UPDATE_VALIDATOR
+        )
+
+        # Entry with only additions should be valid
+        validate_did_update_entry_format(
+            ["DIDUpdate", "1.0.0", key_id, "0xaffe"],
+            {
+                "add": {
+                    "managementKey": [
+                        ManagementKey(
+                            alias="my-man-key-1",
+                            controller=did,
+                            signature_type=SignatureType.EdDSA.value,
+                            priority="0",
+                        ).to_entry_dict(did),
+                        ManagementKey(
+                            alias="my-man-key-2",
+                            controller=did,
+                            signature_type=SignatureType.RSA.value,
+                            priority="1",
+                        ).to_entry_dict(did),
+                    ],
+                    "didKey": [
+                        DIDKey(
+                            alias="my-did-key",
+                            controller=did,
+                            signature_type=SignatureType.RSA.ECDSA.value,
+                            purpose=DIDKeyPurpose.PublicKey.value,
+                        ).to_entry_dict(did)
+                    ],
+                }
+            },
+            self.DID_UPDATE_VALIDATOR,
+        )
+
+        # Entry with only revocations should be valid
+        validate_did_update_entry_format(
+            ["DIDUpdate", "1.0.0", key_id, "0xaffe"],
+            {
+                "revoke": {
+                    "managementKey": [{"id": "management-key-1"}],
+                    "didKey": [
+                        {"id": "did-key-1"},
+                        {"id": "did-key-2", "purpose": ["authentication"]},
+                    ],
+                    "service": [{"id": "service-1"}],
+                }
+            },
+            self.DID_UPDATE_VALIDATOR,
+        )
+
+        # Entry with both additions and revocations should be valid
+        validate_did_update_entry_format(
+            ["DIDUpdate", "1.0.0", key_id, "0xaffe"],
+            {
+                "add": {
+                    "managementKey": [
+                        ManagementKey(
+                            alias="my-man-key-1",
+                            controller=did,
+                            signature_type=SignatureType.EdDSA.value,
+                            priority="0",
+                        ).to_entry_dict(did),
+                        ManagementKey(
+                            alias="my-man-key-2",
+                            controller=did,
+                            signature_type=SignatureType.RSA.value,
+                            priority="1",
+                        ).to_entry_dict(did),
+                    ],
+                    "didKey": [
+                        DIDKey(
+                            alias="my-did-key",
+                            controller=did,
+                            signature_type=SignatureType.RSA.ECDSA.value,
+                            purpose=DIDKeyPurpose.PublicKey.value,
+                        ).to_entry_dict(did)
+                    ],
+                    "service": [
+                        Service(
+                            alias="gmail-service",
+                            service_type="email-service",
+                            endpoint="https://gmail.com",
+                        ).to_entry_dict(did)
+                    ],
+                },
+                "revoke": {
+                    "managementKey": [{"id": "management-key-1"}],
+                    "didKey": [
+                        {"id": "did-key-1"},
+                        {"id": "did-key-2", "purpose": ["publicKey"]},
+                    ],
+                    "service": [{"id": "service-1"}],
+                },
+            },
+            self.DID_UPDATE_VALIDATOR,
         )
