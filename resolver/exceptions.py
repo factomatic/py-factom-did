34043114edac485eb3a72472e46474d48a5ef516
Valentin Ganev <sanchopansa@gmail.com>
2019-09-20 22:49:09 +0300

    refactor validators

--- resolver/exceptions.py
+++ resolver/exceptions.py
@@ -2,17 +2,5 @@ class MalformedDIDManagementEntry(Exception):
     pass
 
 
-class MalformedDIDUpdateEntry(Exception):
-    pass
-
-
-class MalformedDIDMethodVersionUpgradeEntry(Exception):
-    pass
-
-
-class MalformedDIDDeactivationEntry(Exception):
-    pass
-
-
 class InvalidDIDChain(Exception):
     pass
--- resolver/validators.py
+++ resolver/validators.py
@@ -2,202 +2,145 @@ import re
 
 from client.constants import DID_METHOD_NAME
 from client.enums import EntryType
-from resolver.exceptions import (
-    MalformedDIDDeactivationEntry,
-    MalformedDIDManagementEntry,
-    MalformedDIDMethodVersionUpgradeEntry,
-    MalformedDIDUpdateEntry,
-)
-
-from jsonschema.exceptions import ValidationError
+from resolver.exceptions import MalformedDIDManagementEntry
 
 
-# TODO: Split validators in ExtID validator and content validator
-def validate_did_management_entry_format(ext_ids, content, schema_validator):
+class DeactivationEntryContentValidator:
+    @staticmethod
+    def validate(content):
+        if content:
+            return False
+        return True
+
+
+def validate_did_management_ext_ids_v100(ext_ids):
     """
-    Validates the format of a DIDManagement entry.
+    Validates the ExtIDs of a DIDManagement entry.
 
     Parameters
     ----------
     ext_ids: list of str
         The ExtIDs of the entry
-    content: dict
-        The entry content
-    schema_validator: jsonschema.validators.Draft7Validator
-        The entry content schema validator
 
     Raises
     ------
     MalformedDIDManagementEntry
-        If the ExtIDs or the entry content do not follow the Factom DID method specification
+        If the ExtIDs are not valid.
     """
 
-    _validate_ext_ids_length(ext_ids, 2, EntryType.Create, MalformedDIDManagementEntry)
-    _validate_entry_type(ext_ids, EntryType.Create, MalformedDIDManagementEntry)
-    _validate_schema_version(ext_ids, EntryType.Create, MalformedDIDManagementEntry)
-    _validate_entry_content(
-        content, EntryType.Create, schema_validator, MalformedDIDManagementEntry
-    )
-
-
-def validate_did_update_entry_format(ext_ids, content, schema_validator):
-    """
-    Validates the format of a DIDUpdate entry.
-
-    Parameters
-    ----------
-    ext_ids: list of str
-        The ExtIDs of the entry
-    content: dict
-        The entry content
-    schema_validator: jsonschema.validators.Draft7Validator
-        The entry content schema validator
-
-    Raises
-    ------
-    MalformedDIDUpdateEntry
-        If the ExtIDs or the entry content do not follow the Factom DID method specification
-    """
-    _validate_ext_ids_length(ext_ids, 4, EntryType.Update, MalformedDIDUpdateEntry)
-    _validate_entry_type(ext_ids, EntryType.Update, MalformedDIDUpdateEntry)
-    _validate_schema_version(ext_ids, EntryType.Update, MalformedDIDUpdateEntry)
-    _validate_key_identifier(ext_ids, EntryType.Update, MalformedDIDUpdateEntry)
-    _validate_signature_format(ext_ids, EntryType.Update, MalformedDIDUpdateEntry)
-    _validate_entry_content(
-        content, EntryType.Update, schema_validator, MalformedDIDUpdateEntry
-    )
-
-
-def validate_did_method_version_upgrade_entry_format(
-    ext_ids, content, schema_validator
-):
-    """
-    Validates the format of a DIDMethodVersionUpgrade entry.
-
-    Parameters
-    ----------
-    ext_ids: list of str
-        The ExtIDs of the entry
-    content: dict
-        The entry content
-    schema_validator: jsonschema.validators.Draft7Validator
-        The entry content schema validator
-
-    Raises
-    ------
-    MalformedDIDMethodVersionUpgradeEntry
-        If the ExtIDs or the entry content do not follow the Factom DID method specification
-    """
-    _validate_ext_ids_length(
-        ext_ids, 4, EntryType.VersionUpgrade, MalformedDIDMethodVersionUpgradeEntry
-    )
-    _validate_entry_type(
-        ext_ids, EntryType.VersionUpgrade, MalformedDIDMethodVersionUpgradeEntry
-    )
-    _validate_schema_version(
-        ext_ids, EntryType.VersionUpgrade, MalformedDIDMethodVersionUpgradeEntry
-    )
-    _validate_key_identifier(
-        ext_ids, EntryType.VersionUpgrade, MalformedDIDMethodVersionUpgradeEntry
-    )
-    _validate_signature_format(
-        ext_ids, EntryType.VersionUpgrade, MalformedDIDMethodVersionUpgradeEntry
-    )
-    _validate_entry_content(
-        content,
-        EntryType.VersionUpgrade,
-        schema_validator,
-        MalformedDIDMethodVersionUpgradeEntry,
-    )
-
-
-def validate_did_deactivation_entry_format(ext_ids, content):
-    """
-    Validates the format of a DIDDeactivation entry.
-
-    Parameters
-    ----------
-    ext_ids: list of str
-        The ExtIDs of the entry
-    content: dict
-        The entry content
-
-    Raises
-    ------
-    MalformedDIDDeactivationEntry
-        If the ExtIDs or the entry content do not follow the Factom DID method specification
-    """
-    _validate_ext_ids_length(
-        ext_ids, 4, EntryType.Deactivation, MalformedDIDDeactivationEntry
-    )
-    _validate_entry_type(ext_ids, EntryType.Deactivation, MalformedDIDDeactivationEntry)
-    _validate_schema_version(
-        ext_ids, EntryType.Deactivation, MalformedDIDDeactivationEntry
-    )
-    _validate_key_identifier(
-        ext_ids, EntryType.Deactivation, MalformedDIDDeactivationEntry
-    )
-    _validate_signature_format(
-        ext_ids, EntryType.Deactivation, MalformedDIDDeactivationEntry
-    )
-
-    # The content of a DIDDeactivation entry must be empty
-    if content:
-        raise MalformedDIDDeactivationEntry(
-            "Malformed {} entry content".format(EntryType.Deactivation.value)
+    if all(
+        [
+            _validate_ext_ids_length(ext_ids, 2),
+            _validate_entry_type(ext_ids, EntryType.Create),
+            _validate_schema_version(ext_ids),
+        ]
+    ):
+        return True
+    else:
+        raise MalformedDIDManagementEntry(
+            "Invalid or missing {} entry ExtIDs".format(EntryType.Create.value)
         )
 
 
-def _validate_ext_ids_length(ext_ids, min_length, entry_type, exception):
+def validate_did_update_ext_ids_v100(ext_ids):
+    """
+    Validates the ExtIDs of a DIDUpdate entry.
+
+    Parameters
+    ----------
+    ext_ids: list of str
+        The ExtIDs of the entry
+
+    Returns
+    -------
+    bool
+        True if the ExtIDs are valid, False otherwise.
+    """
+    return (
+        _validate_ext_ids_length(ext_ids, 4)
+        and _validate_entry_type(ext_ids, EntryType.Update)
+        and _validate_schema_version(ext_ids)
+        and _validate_key_identifier(ext_ids)
+        and _validate_signature_format(ext_ids)
+    )
+
+
+def validate_did_method_version_upgrade_ext_ids_v100(ext_ids):
+    """
+    Validates the ExtIDs of a DIDMethodVersionUpgrade entry.
+
+    Parameters
+    ----------
+    ext_ids: list of str
+        The ExtIDs of the entry
+
+    Returns
+    -------
+    bool
+        True if the ExtIDs are valid, False otherwise.
+    """
+    return (
+        _validate_ext_ids_length(ext_ids, 4)
+        and _validate_entry_type(ext_ids, EntryType.VersionUpgrade)
+        and _validate_schema_version(ext_ids)
+        and _validate_key_identifier(ext_ids)
+        and _validate_signature_format(ext_ids)
+    )
+
+
+def validate_did_deactivation_ext_ids_v100(ext_ids):
+    """
+    Validates the ExtIDs of a DIDDeactivation entry.
+
+    Parameters
+    ----------
+    ext_ids: list of str
+        The ExtIDs of the entry
+
+    Returns
+    -------
+    bool
+        True if the ExtIDs are valid, False otherwise.
+    """
+    return (
+        _validate_ext_ids_length(ext_ids, 4)
+        and _validate_entry_type(ext_ids, EntryType.Deactivation)
+        and _validate_schema_version(ext_ids)
+        and _validate_key_identifier(ext_ids)
+        and _validate_signature_format(ext_ids)
+    )
+
+
+def _validate_ext_ids_length(ext_ids, min_length):
     if len(ext_ids) < min_length:
-        raise exception(
-            "{} entry must have at least {} ExtIDs".format(entry_type.value, min_length)
-        )
+        return False
+    return True
 
 
-def _validate_entry_type(ext_ids, entry_type, exception):
+def _validate_entry_type(ext_ids, entry_type):
     if ext_ids[0] != entry_type.value:
-        raise exception(
-            "First ExtID of {} entry must be {}".format(
-                entry_type.value, entry_type.value
-            )
-        )
+        return False
+    return True
 
 
-def _validate_schema_version(ext_ids, entry_type, exception):
+def _validate_schema_version(ext_ids):
     if re.match(r"^\d+\.\d+\.\d+$", ext_ids[1]) is None:
-        raise exception(
-            "Second ExtID of {} entry must be a semantic version number".format(
-                entry_type.value
-            )
-        )
+        return False
+    return True
 
 
-def _validate_key_identifier(ext_ids, entry_type, exception):
+def _validate_key_identifier(ext_ids):
     if (
         re.match(
             r"^{}:[0-9a-f]{{64}}#[a-zA-Z0-9-]+$".format(DID_METHOD_NAME), ext_ids[2]
         )
         is None
     ):
-        raise exception(
-            "Third ExtID of {} entry must be a valid full key identifier".format(
-                entry_type.value
-            )
-        )
+        return False
+    return True
 
 
-def _validate_signature_format(ext_ids, entry_type, exception):
+def _validate_signature_format(ext_ids):
     if re.match(r"^0x[0-9a-f]+$", ext_ids[3]) is None:
-        raise exception(
-            "Fourth ExtID of {} entry must be a hex string with leading 0x".format(
-                entry_type.value
-            )
-        )
-
-
-def _validate_entry_content(content, entry_type, schema_validator, exception):
-    try:
-        schema_validator.validate(content)
-    except ValidationError:
-        raise exception("Malformed {} entry content".format(entry_type.value))
+        return False
+    return True
